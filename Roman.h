a5facf85b33f7dc00b320d4962db657f9682fb2c
TrevorNewey <31452349+TrevorNewey@users.noreply.github.com>
2017-10-29 21:39:35 -0600

    Slight update. Added Class Roman. With private and public assess.

--- Roman.h
+++ Roman.h
@@ -4,12 +4,20 @@
 
 #ifndef HOUSE_GRAYJOY_HW5_TASK1_ROMAN_H
 #define HOUSE_GRAYJOY_HW5_TASK1_ROMAN_H
-
+#include <iostream>
+#include <string>
+using namespace std;
 
 class Roman
 {
+    private:
+        unsigned int value;
+    public:
+        Roman(): value(0){};   //This allows us to make empty objects. (Default Constructor)
+        Roman(string romNum): value(0){};           //This takes Roman numeral as a string.
+        int convertFromRoman(){
 
+        }
 };
 
-
-#endif //HOUSE_GRAYJOY_HW5_TASK1_ROMAN_H
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+#include "Roman.h"
+using namespace std;
 
+//void testConstructor();
+//void testOperatorPlus();
+//void testOperatorPlusEqual();
+//void testOperatorIncrement();
+//void testConsoleIO();
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //testConstructor();
+    //testOperatorPlus();
+    //testOperatorPlusEqual();
+    //testOperatorIncrement();
+    //testOutput();
+    Roman r1("XIV");
+    r1.convertFromRoman();
     return 0;
 }
